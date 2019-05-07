e50887937ac0ecf0d06cda43a71deef6972b3f92
Anderson Henrique Gonçalves <anderson.hg25@gmail.com>
2019-05-07 16:10:31 -0300

    erro de cadastro

--- src/views/Autenticacao/Cadastro/Cadastro.js
+++ src/views/Autenticacao/Cadastro/Cadastro.js
@@ -104,12 +104,14 @@ class Cadastro extends Component {
           Swal.fire({html:'<p>'+this.state.message+' <br /> <b>'+ this.state.tipoErro +'</b> </p>', type: 'error', showConfirmButton: true});
         }
       }).catch((error) => {
+        if (error !== ''){
         if (error.response.data.data.email !== '' || error.response.data.data.email !== 'undefined'){
           this.setState({tipoErro: error.response.data.data.email});
         }else if(error.response.data.data._schema !== '' || error.response.data.data._schema !== 'undefined'){
           this.setState({tipoErro: error.response.data.data._schema});
         }else{
           this.setState({tipoErro: "Erro desconhecido, tente novamente!"});
+        }
         }
         Swal.fire({html:'<p>'+ error.response.data.message+'<br />'+ this.state.tipoErro +'</p>', type: 'error', showConfirmButton: false, showCancelButton: true, cancelButtonText: 'Fechar'});
     });
