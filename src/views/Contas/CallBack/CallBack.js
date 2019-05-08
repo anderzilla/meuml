df227b6ba7ecdf01aee7c556b6727c5fdbd5bbff
Anderson Henrique Gonçalves <anderson.hg25@gmail.com>
2019-05-08 16:52:07 -0300

    Limpando consolelog do Callback

--- src/views/Contas/CallBack/CallBack.js
+++ src/views/Contas/CallBack/CallBack.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-//import { Button, Card, CardBody, CardHeader, CardFooter, Col, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
-//import { Link } from 'react-router-dom';
+import { Component } from 'react';
 import {getToken} from '../../../auth';
 import axios from 'axios';
 import Swal from 'sweetalert2';
@@ -9,13 +7,7 @@ class CallBack extends Component {
 
   constructor(props) {
     super(props);
-    
-    //const search = props.location.search;
-    //const query = new URLSearchParams(search);
-    const token = window.location.href.split('?')[1].split('=')[1]
-    //query.get('code')
-    console.log(token)//retorno do mercado livre
-    
+    const token = window.location.href.split('?')[1].split('=')[1];
     this.state = {
       token: token,
       message: '',
@@ -29,39 +21,22 @@ class CallBack extends Component {
       if (res.data.status === 'success'){
         Swal.fire({html:'<p>'+res.data.message+'</p>', type: 'success', showConfirmButton: true,
         onClose: () => {
-          //console.log(res.data.status);
           this.props.history.push('/listacontas');
-          //window.location.reload();
         }});
       }else{
        Swal.fire({html:'<p>'+res.data.message+'</p>', type: 'error', showConfirmButton: true,
        onClose: () => {
-        //console.log(res.data.message); 
         this.props.history.push('/listacontas');
-         //window.location.reload();
        }});
       }
     }).catch(error => {
-      Swal.fire({html:'<p>ErroGeral::'+ error+'</p>', type: 'error', showConfirmButton: false, showCancelButton: true, cancelButtonText: 'Fechar',
+      Swal.fire({html:'<p>'+ error.response.data.message+'</p>', type: 'error', showConfirmButton: false, showCancelButton: true, cancelButtonText: 'Fechar',
       onClose: () => {
-        //console.log(error);
         this.props.history.push('/listacontas');
-        //window.location.reload();
       }});
     });
 
   }
-
-  render(){
-    
-    return(
-      <div className="app flex-row align-items-center">
-      <p>{this.state.token}</p>
-      </div>
-    );
-  }
-  
-
 }
 
 export default CallBack;
