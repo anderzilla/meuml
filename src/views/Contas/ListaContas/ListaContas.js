7c6d9b73b18845d5e499897751eab2cf21fda662
Anderson Henrique Gonçalves <anderson.hg25@gmail.com>
2019-05-09 15:52:52 -0300

    Limpando comentários do código lista contas

--- src/views/Contas/ListaContas/ListaContas.js
+++ src/views/Contas/ListaContas/ListaContas.js
@@ -26,14 +26,7 @@ class ListaContas extends Component {
       page: '',
       totalPages: '',
       limit: '',
-    }
-    //Constantes para serem utilizadas na montagem da lista
-    /*const contas = [];
-    const total = ''; 
-    const page = ''; 
-    const totalPages = ''; 
-    const limit = '';*/
-    
+    } 
     axios.get(`https://api.app2.meuml.com/accounts`, { headers: {"Authorization" : 'Bearer '+getToken()}
     })
     .then(res => {
@@ -41,24 +34,14 @@ class ListaContas extends Component {
         const message = res.data.message;
         if (res.data.meta.total !== 0){
           //DADOS DAS CONTAS
-          //contas = res.data.data;
           this.setState({contas: res.data.data});
-          //total = res.data.meta.total; 
           this.setState({total:res.data.meta.total});
-          //page = res.data.meta.page; 
           this.setState({page: res.data.meta.page});
-          //totalPages = res.data.meta.pages; 
           this.setState({totalPages: res.data.meta.pages});
-          //limit = res.data.meta.limit;
           this.setState({limit: res.data.meta.limit});
-
-          
-
         }else{
           Swal.fire({html:'<p>'+message+'</p>', type: 'info', showConfirmButton: true});  
-        }
-        
-        
+        }  
       }else{
         Swal.fire({html:'<p>'+res.data.message+'</p>', type: 'error', showConfirmButton: true});
       }
@@ -121,48 +104,6 @@ class ListaContas extends Component {
   }
 
   render() {
-    /* CARREGALISTAGEM DE CONTAS ML 
-    let lista = []; //this.state.contas;
-    for(let i=0; i< this.state.total; i++){
-      xcontas.push(
-        <Col xs="12" sm="6" md="3">
-        <Card className="card-accent-primary">
-          <CardHeader>
-            {xcontas.external_data.user_type}
-            <div class="float-right">
-            <Dropdown isOpen={this.state.dropdownOpen[{i}]} toggle={() => {this.toggle({i});}} size="sm" class="sm-info">
-              <DropdownToggle caret color="primary">
-                Opções
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem><Link to="/renomearconta/{contas.external_data.id}"><i class="fa fa-edit"></i> Renomear</Link></DropdownItem>
-                <DropdownItem><Link to="/sincronizarConta/{contas.external_data.id}"><i class="fa fa-refresh"></i> Sincronizar</Link></DropdownItem>
-                <DropdownItem><Link to="/excluirconta/{contas.external_data.id}"><i class="fa fa-remove"></i> Excluir</Link></DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-            </div>
-          </CardHeader>
-          <CardBody>
-            <img src={sygnet} class="img-full70 align-content-center" alt="Loja Teste"></img>
-            <p class="text-primary h5 text-center">{xcontas.name}</p>
-            <p class="text-left">
-            <i class="fa fa-envelope"></i> E-mail: {xcontas.external_data.email}<br></br>
-            <i class="fa fa-user"></i> Usuário: {xcontas.external_data.nickname}
-            </p>
-          </CardBody>
-          <CardFooter>
-          <div class="text-left float-left">
-          <span class="text-success h5">{xcontas.seller_reputation.transactions.completed}</span> Vendas
-          </div>
-          <div class="float-right text-right">
-          <span class="text-success h5"></span> Anúncios
-          </div>
-          </CardFooter>
-        </Card>
-      </Col>
-      )
-    }*/
-
     return (
       <div className="animated fadeIn">
         <h1>Contas
