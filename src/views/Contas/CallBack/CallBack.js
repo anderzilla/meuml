e3fc28e0d57c911dae32545e3accd5b2169f76a9
Anderson Henrique Gonçalves (AnderZilla) <anderson.hg25@gmail.com>
2019-05-13 15:46:38 +0000

    Resolve "Contas: ajustes finais"

--- src/views/Contas/CallBack/CallBack.js
+++ src/views/Contas/CallBack/CallBack.js
@@ -32,20 +32,11 @@ class CallBack extends Component {
        }});
       }
     }).catch(error => {
-      if(error.indexof("response") !== '-1'){
-        Swal.fire({html:'<p>'+ error.response.data.message+'</p>', type: 'error', showConfirmButton: false, showCancelButton: true, cancelButtonText: 'Fechar',
+      Swal.fire({html:'<p>'+ error +'</p>', type: 'error', showConfirmButton: false, showCancelButton: true, cancelButtonText: 'Fechar',
       onClose: () => {
         this.props.history.push('/listacontas');
         window.location.reload();
       }});
-      }else{
-        Swal.fire({html:'<p>'+ error+'</p>', type: 'error', showConfirmButton: false, showCancelButton: true, cancelButtonText: 'Fechar',
-      onClose: () => {
-        this.props.history.push('/listacontas');
-        window.location.reload();
-      }});
-      }
-      
     });
 
   }
