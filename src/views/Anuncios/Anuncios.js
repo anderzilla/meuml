0c69a1373d855d105a1dda9bdf91c7334cac6e4d
Allan Roscoche <allanroscoche@gmail.com>
2019-05-22 22:00:54 -0300

    added dropdown buttons to anuncios

--- src/views/Anuncios/Anuncios.js
+++ src/views/Anuncios/Anuncios.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { 
+  ButtonDropdown, DropdownMenu, DropdownToggle, DropdownItem,
+  Card, CardBody, Row } from 'reactstrap';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        Anuncios
+        <Card>
+          <CardBody>
+            <Row className="align-items-center">
+              <ButtonDropdown isOpen={true} >
+                <DropdownToggle caret>
+                  Conta
+                </DropdownToggle>
+                <DropdownMenu left>
+                  <DropdownItem >Conta 1</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+              <ButtonDropdown >
+                <DropdownToggle caret>
+                  Status
+                </DropdownToggle>
+              </ButtonDropdown>
+ 
+            </Row>
+          </CardBody>
+        </Card>
       </div>
     );
   }
