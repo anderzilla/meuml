5fd2651317ffe26c630beb705b109d3e8c6e8c6c
Ronaldo <ronaldovargas@outlook.com>
2019-07-02 19:28:55 -0300

    correção bug session

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,5 @@
+
+import moment from 'moment';
 export const TOKEN_KEY = "@MeuML-Token";
 export const TOKEN_EXPIRE_IN = "@MeuML-Token-expire";
 export const USER_ID = "@MeuML-UserId";
@@ -6,7 +8,29 @@ export const USER_EMAIL = "@MeuML-UserEmail";
 export const USER_SELLER_ID = "@MeuML-UserSellerId";
 export const API = "https://api.app2.meuml.com";
 
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+export const isAuthenticated = () => {
+
+  debugger
+//  if(localStorage.getItem(TOKEN_KEY) !== null;
+
+//const expireToken = localStorage.getItem("@MeuML-Token-expire");
+const expireToken = "02/07/2019 19:35"
+
+const dataFim = moment().format("DD/MM/YYYY HH:mm")
+
+var ms = moment(expireToken).diff(dataFim)
+
+if(ms > 0)
+{
+  if(localStorage.getItem(TOKEN_KEY) !== null){
+    return true
+  }
+
+}else{
+return false
+}
+
+}
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token,expire) => {
   localStorage.setItem(TOKEN_KEY, token);
