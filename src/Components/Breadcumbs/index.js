bf47b97b651744c15c61117e8efc07907aa52d7f
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-07-09 11:20:21 -0300

    empty file

---
+++ src/Components/Breadcumbs/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Breadcumbs extends React.Component {
+    render() {
+        return(
+            <div>
+                Breadcumbs
+            </div>
+        );
+    }
+}
