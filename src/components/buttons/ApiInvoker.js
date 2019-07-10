595198de49ee0afb51ab4e4cc9feb5bc12a16187
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-07-10 18:20:44 -0300

    mvf-21-criar-componente-button-group-vertical

---
+++ src/components/buttons/ApiInvoker.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { apiGet, apiPost, apiPut, apiDelete } from './api-lexicon';
+import swalShout from '../../helpers/swalShout';
+
+class ApiInvoker extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            url: '',
+            data: null,
+            http: '',
+            onSuccess: '',
+            hasChanged: false
+        }
+    }
+
+    callback(props) {
+        if(props.http === 'get' || props.http === 'delete') {
+            this.setState({
+                url: props.url, // Delete parameters must be passed through URL
+                data: null,
+                http: props.http,
+                onSuccess: props.onSuccess,
+                hasChanged: true
+            });
+
+            this.apiHandler(props.http);
+
+        }
+
+        else if(props.http === 'post') {
+            this.setState({
+                url: props.url,
+                data: props.data,
+                http: props.http,
+                onSuccess: props.onSuccess,
+                hasChanged: true
+            });
+
+            this.apiHandler('post');
+
+        }
+
+        else if(props.http === 'put') {
+            this.setState({
+                url: props.url,
+                data: props.data,
+                http: props.http,
+                onSuccess: props.onSuccess,
+                hasChanged: true
+            });
+
+            this.apiHandler('put');
+
+        } else {
+            swalShout(`Método HTTP (${props.http}) inválido`);
+        }
+    };
+
+    async apiHandler(req) {
+        const { url, data, onSuccess, hasChanged } = this.state;
+        if(hasChanged) {
+            if(req === 'get') {
+                apiGet(url, onSuccess);
+            }
+
+            else if(req === 'post') {
+                apiPost(url, data, onSuccess);
+            }
+
+            else if(req === 'put') {
+                apiPut(url, data, onSuccess);
+            }
+
+            else if(req === 'delete') {
+                apiDelete(url, onSuccess)
+            }
+        }
+    };
+
+  render() {
+    return(
+        <button type="button"
+          className={this.props.className}
+          url={this.props.url}
+          data={this.props.data}
+          http={this.props.http}
+          onSuccess={this.props.onSuccess}
+          question={this.props.question}
+          onClick={ ()=> this.callback(this.props) }
+          >{this.props.children}
+        </button>
+    );
+  }
+}
+
+export default ApiInvoker;
