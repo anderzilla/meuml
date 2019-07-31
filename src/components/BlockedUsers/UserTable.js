2f79f999585d3dac2501b778f3a966b6e5202200
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-07-31 17:34:15 -0300

    mvf-102 creation of table dinamic component

--- src/components/BlockedUsers/UserTable.js
+++ src/components/BlockedUsers/UserTable.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Table,
+  Row,
+  Col,
+  Button,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 export default class UserTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.headBuilder = this.headBuilder.bind(this);
+    this.bodyBuilder = this.bodyBuilder.bind(this);
+  }
+
+  headBuilder = () => {
+    if(this.props.thead !== undefined &&
+      this.props.thead !== null &&
+      this.props.thead !== '') {
+        return this.props.thead.map(col=>{return <th>{col}</th>})
+      }
+      else return <div />
+  }
+
+  bodyBuilder = () => {
+    if(this.props.tbody !== undefined &&
+      this.props.tbody !== null &&
+      this.props.tbody !== '') {
+        return this.props.tbody.map(col=>{});
+      }
+      else return <div />
+  }
+
   render() {
     return(
       <>
-        MeusBloqueios
+        <Table responsive>
+          <thead>
+            <tr>
+              {this.headBuilder}
+            </tr>
+          </thead>
+          <tbody>
+            {this.bodyBuilder}
+          </tbody>
+        </Table>
       </>
     );
   }
