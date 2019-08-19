b56ab8d78a95267c290b60f02dac92c623041738
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-08-19 16:12:26 -0300

    first commit

---
+++ src/views/Inicio/Logo/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './logo.css';
+import logo from "../../../assets/img/Logo-transparente.png";
+const Logo = props => <img src={logo} width={props.width} id="logo" />;
+export default Logo;
